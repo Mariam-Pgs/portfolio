795035e01a33f1cb7d2ad483ee6e0459cb541a81
mariampoghossian <poghossianm@gmail.com>
2025-04-14 16:56:53 +0200

    aggrandissement logo dans footer suppression logo insta inutile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 // ✅ src/components/Footer.tsx
-import { FaLinkedin, FaInstagram, FaGithub, FaEnvelope } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 
 
 export default function Footer() {
@@ -7,36 +7,28 @@ export default function Footer() {
    <footer className="bg-gray-200 py-6 mt-20">
      <div className="flex justify-center gap-6">
        <a
-         href="https://www.linkedin.com/in/ton-profil"
+         href="https://www.linkedin.com/in/mariam-poghossian-8482b12ba/"
          target="_blank"
          rel="noopener noreferrer"
          aria-label="LinkedIn"
-         className="text-gray-700 hover:text-purple-600 text-xl"
+         className="text-gray-700 hover:text-purple-600 text-3xl"
        >
          <FaLinkedin />
        </a>
+
        <a
-         href="https://www.instagram.com/tonpseudo"
-         target="_blank"
-         rel="noopener noreferrer"
-         aria-label="Instagram"
-         className="text-gray-700 hover:text-purple-600 text-xl"
-       >
-         <FaInstagram />
-       </a>
-       <a
-         href="https://github.com/ton-github"
+         href="https://github.com/Mariam-Pgs"
          target="_blank"
          rel="noopener noreferrer"
          aria-label="GitHub"
-         className="text-gray-700 hover:text-purple-600 text-xl"
+         className="text-gray-700 hover:text-purple-600 text-3xl"
        >
          <FaGithub />
        </a>
        <a
          href="mailto:poghossianm@gmail.com"
          aria-label="Email"
-         className="text-gray-700 hover:text-purple-600 text-xl"
+         className="text-gray-700 hover:text-purple-600 text-3xl"
        >
          <FaEnvelope />
        </a>

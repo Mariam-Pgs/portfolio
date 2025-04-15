2bcf9651f68a9ccfbac4d1839559907c96bfd7be
Mariam-Pgs <poghossianm@gmail.com>
2025-04-16 00:01:46 +0200

    Update Navbar.tsx (modif taille de police pour telephone-test)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,18 +12,50 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-white/80 backdrop-blur z-50 shadow-sm flex justify-between items-center px-8 py-4">
-<h1
-  className="text-xl font-bold cursor-pointer hover:text-purple-600 hover:drop-shadow-[0_0_15px_rgba(147,51,234,0.8)] transition duration-300"
-  onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
->
-  Accueil
-</h1>
-      <ul className="flex gap-6 text-sm">
-        <li><a href="#about" onClick={(e) => handleScroll(e, "about")} className="hover:text-purple-600">À propos</a></li>
-        <li><a href="#skills" onClick={(e) => handleScroll(e, "skills")} className="hover:text-purple-600">Compétences</a></li>
-        <li><a href="#projects" onClick={(e) => handleScroll(e, "projects")} className="hover:text-purple-600">Projets</a></li>
-        <li><a href="#contact" onClick={(e) => handleScroll(e, "contact")} className="hover:text-purple-600">Contact</a></li>
+    <nav className="fixed top-0 left-0 right-0 bg-white/80 backdrop-blur z-50 shadow-sm flex justify-between items-center px-6 py-3">
+      <h1
+        className="text-xl sm:text-base font-bold cursor-pointer hover:text-purple-600 hover:drop-shadow-[0_0_15px_rgba(147,51,234,0.8)] transition duration-300"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      >
+        Accueil
+      </h1>
+      <ul className="flex gap-4 sm:gap-2 text-sm sm:text-xs">
+        <li>
+          <a
+            href="#about"
+            onClick={(e) => handleScroll(e, "about")}
+            className="hover:text-purple-600"
+          >
+            À propos
+          </a>
+        </li>
+        <li>
+          <a
+            href="#skills"
+            onClick={(e) => handleScroll(e, "skills")}
+            className="hover:text-purple-600"
+          >
+            Compétences
+          </a>
+        </li>
+        <li>
+          <a
+            href="#projects"
+            onClick={(e) => handleScroll(e, "projects")}
+            className="hover:text-purple-600"
+          >
+            Projets
+          </a>
+        </li>
+        <li>
+          <a
+            href="#contact"
+            onClick={(e) => handleScroll(e, "contact")}
+            className="hover:text-purple-600"
+          >
+            Contact
+          </a>
+        </li>
       </ul>
     </nav>
   );

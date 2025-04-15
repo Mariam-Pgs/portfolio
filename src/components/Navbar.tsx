7b9474df96595c238516a4303f56ea92300953e6
Mariam-Pgs <poghossianm@gmail.com>
2025-04-16 00:03:50 +0200

    Update Navbar.tsx (changement taille police-test2)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-// ✅ src/components/Navbar.tsx
 "use client";
 import { MouseEvent } from "react";
 
@@ -12,14 +11,14 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-white/80 backdrop-blur z-50 shadow-sm flex justify-between items-center px-6 py-3">
+    <nav className="fixed top-0 left-0 right-0 bg-white/80 backdrop-blur z-50 shadow-sm flex justify-between items-center px-4 py-3">
       <h1
-        className="text-xl sm:text-base font-bold cursor-pointer hover:text-purple-600 hover:drop-shadow-[0_0_15px_rgba(147,51,234,0.8)] transition duration-300"
+        className="text-base md:text-xl font-bold cursor-pointer hover:text-purple-600 hover:drop-shadow-[0_0_15px_rgba(147,51,234,0.8)] transition duration-300"
         onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
       >
         Accueil
       </h1>
-      <ul className="flex gap-4 sm:gap-2 text-sm sm:text-xs">
+      <ul className="flex gap-3 text-xs md:text-sm">
         <li>
           <a
             href="#about"
